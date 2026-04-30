0bdb89dbc5004b70bfe89b3989021e1ebf4ad680
kofta999 <mostafaxxx555@gmail.com>
2026-04-30 13:35:52 +0300

    chore: adjust tower-http log level to info

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() -> Result<(), AppError> {
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| EnvFilter::new("kubemal=info,kube=info,tower_http=debug")),
+                .unwrap_or_else(|_| EnvFilter::new("kubemal=info,kube=info,tower_http=info")),
         )
         .with_target(false)
         .compact()
